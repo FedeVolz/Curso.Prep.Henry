9f96822d7d979dd20e2621812955dcb12488964f
federico volz <fede_volz@hotmail.com>
2021-06-25 18:59:32 -0300

    homework done

---
+++ 03-JS-II/homework/Switch y Do while.js
@@ -0,0 +1,12 @@
+Switch:
+La declaración switch evalúa una expresión, 
+comparando el valor de esa expresión con una instancia case, 
+y ejecuta declaraciones asociadas a ese case, 
+así como las declaraciones en los case que siguen.
+
+Do...While:
+La sentencia (hacer mientras) crea un bucle que ejecuta una sentencia especificada, 
+hasta que la condición de comprobación se evalúa como falsa. 
+La condición se evalúa después de ejecutar la sentencia, 
+dando como resultado que la sentencia especificada se ejecute al menos una vez.
+
